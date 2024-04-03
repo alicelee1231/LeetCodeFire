c55e046a64381b1487da834d5fa67fd8b08d23a8
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-04-03 18:46:06 +0900

    Time: 455 ms (46.88%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,19 +1,12 @@
-# Write your MySQL query statement below
-with a as (
-    select machine_id, sum(timestamp) as pl
-    from activity
-    where activity_type = 'start'
-    group by machine_id
-), b as (
-    select machine_id, sum(timestamp)as pl2
-    from activity
-    where activity_type = 'end'
+
+    
+    select
+    a1.machine_id,
+    round(avg(a1.timestamp - a.timestamp),3)as processing_time
+    from activity a
+    join activity a1
+    on a1.machine_id = a.machine_id
+    and a1.process_id = a.process_id
+    and a1.activity_type = 'end'
+    and a.activity_type = 'start'
     group by 1
-), c as(
-    select machine_id,count(process_id) /2 as cb
-    from activity
-    group by 1
-)
-select distinct b.machine_id,ROUND((b.pl2 - a.pl) / c.cb,3) as processing_time 
-from b, a ,c
-where a.machine_id = b.machine_id and b.machine_id = c.machine_id
