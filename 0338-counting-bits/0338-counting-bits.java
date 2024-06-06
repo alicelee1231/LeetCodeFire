bd26e6717bf3fb1f4f86f61b8da7ba3920512730
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-06-06 19:02:42 +0900

    Time: 7 ms (19.41%), Space: 46.7 MB (27.35%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            result[i] = countSetBits(i);
+        }
+        return result;
+    }
+    
+    private int countSetBits(int num) {
+        int count = 0;
+        while (num > 0) {
+            count += num & 1;
+            num >>= 1;
+        }
+        return count;
+    }
+}
