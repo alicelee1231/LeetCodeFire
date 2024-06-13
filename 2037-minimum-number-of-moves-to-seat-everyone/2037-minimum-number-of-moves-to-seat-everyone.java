e655795beb1746224369b0c5b9cf9e091ffec21f
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-06-14 00:06:18 +0900

    Time: 3 ms (62.42%), Space: 43.7 MB (94.43%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -4,16 +4,6 @@ class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
         ArrayList<Integer> list = new ArrayList<>();
         int result = 0;
-//         for(int i : seats){
-//             list.add(i);
-//         }
-//         for(int i : students){
-//             list.add(i);
-//         }
-//                 Collections.sort(list);
-//         for(int i =0 ; i < list.size(); i += 2){
-//                 result += list.get(i+1) - list.get(i);            
-//         }
         
         Arrays.sort(seats);
         Arrays.sort(students);
@@ -21,7 +11,6 @@ class Solution {
             result += Math.abs(students[i]- seats[i] ) ;
         }
 
-        
-        return Math.abs(result);
+        return result;
     }
 }
