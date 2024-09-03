6d5dbf8903325dade05f9391cd847d700b335b07
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-09-03 23:21:08 +0900

    Time: 71 ms (27.37%), Space: 53.5 MB (35.18%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(head === null) return false;
+    
+    let tor = head;
+    let hare = head
+    
+    while(hare && hare.next){
+        tor = tor.next;
+        hare = hare.next.next;
+        if(tor === hare)
+        return true
+    }
+    return false
+};
