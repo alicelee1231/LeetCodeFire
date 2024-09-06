217fd76e63e31dbdf817d909bb3043701ea6c4b3
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-09-06 09:32:24 +0900

    Time: 50 ms (68.95%), Space: 48.5 MB (90.91%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    let result = 0
+    let cur = head
+   while(cur !== null){
+       result = result * 2 + cur.val
+       cur = cur.next
+   }
+    return result
+};
