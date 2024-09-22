b5a10a7a27ab65cdc52be31e684fad6a85685f4d
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-09-22 22:51:31 +0900

    Time: 35 ms (99.49%), Space: 48.7 MB (67.56%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -6,7 +6,7 @@ var lengthOfLastWord = function(s) {
     const sp = s.split(" ")
     let min = 1
     for(let i = sp.length - 1; i >= 0; i--){
-      if(sp[sp.length -min].length === 0){
+      if(sp[sp.length - min].length === 0){
           min++
       }else {
           sp[sp.length - min].length
