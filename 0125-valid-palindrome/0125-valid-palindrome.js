e5c7691dd3f1b150d7b1c31f7fb5fa5330473aab
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-12-17 23:08:38 +0900

    Time: 36 ms (5.00%), Space: 57.8 MB (8.43%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,7 +3,22 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
-    const reversedStr = str.split('').reverse().join('');
-    return str === reversedStr;
-}
+    let b = []
+    let a = s.toLowerCase()
+    for(let i = 0; i< s.length; i++){
+        if(a[i].charCodeAt(0) >= 97 && a[i].charCodeAt(0) <= 122){
+            b.push(a[i])
+        }else if( a[i].charCodeAt(0) >= 48 && a[i].charCodeAt(0) <= 57) {
+            b.push(a[i])
+        }
+    }
+    console.log(b)
+    for(let i = 0 ; i< b.length; i++){
+        if(b[i] !== b[b.length - 1 -i]){
+            return false
+        }else if(b[i] === b[b.length - 1 -i]){
+            continue
+        }
+        }
+        return true
+    };
