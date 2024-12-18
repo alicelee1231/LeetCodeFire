53738b0b7c19ef04723d3535fe718ca0eabee903
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-12-18 11:23:04 +0900

    Time: 14 ms (60.63%), Space: 53.1 MB (81.59%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let b = new Map()
+    let x = []
+    for(let i = 0; i< nums.length; i++){
+       if(b.has(nums[i])){
+           b.set(nums[i],b.get(nums[i]) + 1)
+       }else{
+           b.set(nums[i],1)
+       }
+    }
+    let answer = [...b.entries()]
+    .sort((a, b) => b[1] - a[1])
+    .map((num) => num[0]);
+        console.log(answer)
+
+    let result = answer.slice(0, k )
+    return result
+    };
+
