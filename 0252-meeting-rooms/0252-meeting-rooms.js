3b756f79817eea6299b8309e5521b214561bc9fe
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-12-18 21:30:58 +0900

    Time: 3 ms (92.22%), Space: 51.8 MB (28.73%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.js
+++ 0252-meeting-rooms/0252-meeting-rooms.js
@@ -3,8 +3,11 @@
  * @return {boolean}
  */
 var canAttendMeetings = function(intervals) {
-  
+  //first, to compare each other, I sorted all of the elements.
     intervals.sort((x,y) => x[0] - y[0])
+    
+    // second, after sorting, I just compare the start time with the end time which before arrary.
+// so if the end time is over to next start time, the person can't attend the meeting.
     for(let i = 1; i < intervals.length; i++){
         if(intervals[i][0] < intervals[i-1][1])
             return false
@@ -12,4 +15,4 @@ var canAttendMeetings = function(intervals) {
     return true
 };
 
-//first, 
+
