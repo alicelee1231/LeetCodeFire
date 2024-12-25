2418839e1efcd82b52b42b8e68577b10fea92c44
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-12-25 16:19:24 +0900

    Time: 2 ms (59.60%), Space: 50 MB (82.73%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,12 +4,11 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-  
-   // let x = new Set(nums1)
-   // let y = new Set(nums2)
-   //  let inter = new Set([...x].filter(num => y.has(num)))
-   //  return [...inter]
-    
+
+//  I need to check the same elements where in both array.
+//  so I use filter method and compare to each other
+//     and then get rid of duplicated factor, 
+// I use Set structure and convert to array again for output.
     let a = [...nums1].filter(num => nums2.includes(num))
     let result = new Set(a)
     return [...result]
