234b30a4617880065019f9ed8560e39acc1e196f
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-12-25 16:17:21 +0900

    Time: 2 ms (59.60%), Space: 50.2 MB (81.19%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -5,8 +5,12 @@
  */
 var intersection = function(nums1, nums2) {
   
-   let x = new Set(nums1)
-   let y = new Set(nums2)
-    let inter = new Set([...x].filter(num => y.has(num)))
-    return [...inter]
+   // let x = new Set(nums1)
+   // let y = new Set(nums2)
+   //  let inter = new Set([...x].filter(num => y.has(num)))
+   //  return [...inter]
+    
+    let a = [...nums1].filter(num => nums2.includes(num))
+    let result = new Set(a)
+    return [...result]
 };
