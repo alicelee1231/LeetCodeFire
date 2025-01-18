0bc4576e5dd584191739e9162d370d6f6904aa70
Saem <104006855+alicelee1231@users.noreply.github.com>
2025-01-18 09:33:57 +0900

    Time: 6 ms (45.58%), Space: 51.3 MB (43.12%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -25,29 +25,29 @@ var longestPalindrome = function(s) {
             
         
     //     return cnt
-    let x = s.split('').sort().join('');
-let a = [];
-let b = [...new Set(x)];
+    let x = s.split('').sort().join('')
+        let a = []
+        let b = [...new Set(x)]
+        for(let i = 0; i < b.length;i++){
+            a.push(x.lastIndexOf(b[i]) - x.indexOf(b[i])+1)
+        }
 
-for (let i = 0; i < b.length; i++) {
-    a.push(x.lastIndexOf(b[i]) - x.indexOf(b[i]) + 1);
-}
-
-let cnt = 0;
-let hasOdd = false;
-
-for (let i = 0; i < a.length; i++) {
-    if (a[i] % 2 === 0) {
-        cnt += a[i]; 
-    } else {
-        cnt += a[i] - 1; 
-        hasOdd = true;}
-}
-
-if (hasOdd) {
-    cnt += 1; 
-}
-
-return cnt;
+        let cnt = 0
+        let odd = false
+        for(let i = 0; i < a.length; i++){
+            if(a.length === 1){
+                return a[i]
+            }
+            if(a[i] % 2 === 0){
+                cnt += a[i]
+            }else {
+                cnt += a[i] - 1
+                odd = true
+            }
+        }
+        if(odd){
+                cnt +=1
+            }
+            return cnt
 
 };
