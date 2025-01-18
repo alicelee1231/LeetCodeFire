44a4d845af5ad229c63855fc569162f6e7b9c5e8
Saem <104006855+alicelee1231@users.noreply.github.com>
2025-01-18 09:52:20 +0900

    Time: 7 ms (31.53%), Space: 50.9 MB (61.79%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -3,29 +3,7 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    //  let a = new Map()
-    //     for(let i = 0; i < s.length; i++){
-    //         if(a.has(s[i])){
-    //             a.set(s[i],a.get(s[i]) + 1)
-    //         }else a.set(s[i],1)
-    //     }
-    //     let cnt = 0
-    //     let b = [...new Set(s)]
-    //     for(let i = 0;  i < b.length; i++){
-    //         if(a.get(b[i]) % 2 === 0){
-    //             cnt += a.get(b[i])
-    //         }
-    //         }
-    //         for(let i = 0; i < b.length; i++ ){
-    //             if(a.get(b[i]) === 1){
-    //                 cnt +=1
-    //                 break
-    //             }
-    //         }
-            
-        
-    //     return cnt
-    let x = s.split('').sort().join('')
+        let x = s.split('').sort().join('')
         let a = []
         let b = [...new Set(x)]
         for(let i = 0; i < b.length;i++){
