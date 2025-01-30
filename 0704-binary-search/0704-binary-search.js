79501781962a6941131028d989c90a37f1b71e81
Saem <104006855+alicelee1231@users.noreply.github.com>
2025-01-30 13:07:55 +0900

    Time: 4 ms (5.75%), Space: 52.8 MB (67.23%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,10 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
-    for(let i = 0; i< nums.length; i++){
-        if(nums[i] == target){
-            return i
-        }
-    }return -1
+
+  let left = 0
+  let right = nums.length - 1
+  while(left <= right){
+    let mid = Math.floor((left + right) / 2)
+    console.log(mid)
+    if(nums[mid]===target){
+        return mid
+    }
+    if(nums[mid] < target){
+        left = mid + 1
+    }else if(nums[mid] > target){
+         right = mid - 1
+    }
+  }
+    return -1
 };
