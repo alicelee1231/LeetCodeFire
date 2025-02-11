70bd8f2ae997b671ef8a35547a2420d7a4f8a7e0
Saem <104006855+alicelee1231@users.noreply.github.com>
2025-02-11 23:29:54 +0900

    Time: 20 ms (60.65%), Space: 58.7 MB (74.53%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.js
@@ -0,0 +1,16 @@
+var findLHS = function(nums) {
+    const map = new Map();
+    let maxLen = 0;
+    
+    for (let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+    
+    for (let [num, count] of map) {
+        if (map.has(num + 1)) {
+            maxLen = Math.max(maxLen, count + map.get(num + 1));
+        }
+    }
+    
+    return maxLen;
+};
