3c8bc44a78e6ae6a71a751ce104632201b42a2ec
Alice <104006855+alicelee1231@users.noreply.github.com>
2025-04-11 06:51:38 +0900

    Time: 2 ms (83.22%), Space: 65.2 MB (40.71%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -1,14 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
 var maxProfit = function(prices) {
-    let minPrice = prices[0];
-    let maxProfit = 0;
+   let minP = prices[0]
+   let maxP = 0
 
-    for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < minPrice) {
-            minPrice = prices[i];
-        } else if (prices[i] - minPrice > maxProfit) {
-            maxProfit = prices[i] - minPrice;
-        }
-    }
-
-    return maxProfit;
+   for(let i = 1; i< prices.length; i++){
+    const profit = prices[i] - minP
+    maxP = Math.max(maxP, profit)
+    minP = Math.min(minP, prices[i])
+   }
+return maxP
 };
