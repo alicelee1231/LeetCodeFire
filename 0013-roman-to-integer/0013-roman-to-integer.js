023ba01b18806580eba4443b1f46f98268609d28
Alice <104006855+alicelee1231@users.noreply.github.com>
2025-05-16 07:08:42 +0900

    Time: 90 ms (5.22%), Space: 65.2 MB (5.3%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,28 +3,28 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const romanMap = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    };
-    
-    let result = 0;
-
-    for (let i = 0; i < s.length; i++) {
-        const currentVal = romanMap[s[i]];
-        const nextVal = romanMap[s[i + 1]];
-
-        if (nextVal > currentVal) {
-            result -= currentVal;
-        } else {
-            result += currentVal;
-        }
+    const a = {
+        'I' :1,
+        'V' :5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000,
     }
+    let result = 0
+    for(let i = 0 ;  i< s.length ; i++){
+        if(a[s[i]] === 1 && (a[s[i+1]] === 5 || a[s[i+1]] === 10)){
+            result -= 1
+        }else if (a[s[i]] === 10 && (a[s[i+1]] === 50 || a[s[i+1]] === 100)){
+            result -= 10
+        }else if(a[s[i]] === 100 && (a[s[i+1]] === 500 || a[s[i+1]] === 1000)){
+            result -= 100
+        }else {
+            result += a[s[i]]
+        }
+        console.log(result)
+    }
+            return result
 
-    return result;
 };
