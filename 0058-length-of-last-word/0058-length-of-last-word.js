5ba22cec8eb62d8a2ee0ae7ea16eed63d6baca04
Alice <104006855+alicelee1231@users.noreply.github.com>
2025-05-19 07:09:47 +0900

    Time: 0 ms (100%), Space: 53.4 MB (71.53%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,14 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    const sp = s.split(" ")
-    let min = 1
-    for(let i = sp.length - 1; i >= 0; i--){
-      if(sp[sp.length - min].length === 0){
-          min++
-      }else {
-          sp[sp.length - min].length
-      }
-    }
-        return sp[sp.length - min].length
+    const a = s.trim().split(' ')
+    return a[a.length - 1].length
 };
