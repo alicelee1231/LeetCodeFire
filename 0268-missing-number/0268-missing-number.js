cf88f0688e4c80a1746b8e230fcbd3112ee993ac
Alice <104006855+alicelee1231@users.noreply.github.com>
2025-05-26 07:12:38 +0900

    Time: 0 ms (100%), Space: 54.5 MB (92.39%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,21 +3,10 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-//   first, I need to check which number is missing. 
-//   so, I will sorting the elements in order
-    nums.sort()
-    let x = 0
-    if(nums.length > nums[nums.length - 1]){
-        x = nums.length
-    }
-
-//  actually, I just want to use for loop, and compare each other
-//     like nums[i + 1] - nums[i] > 1 -> x = nums[i] + 1
-// but I want to approach in other way. so I use indexOf method and search the element.
-    for(let i = 0; i < nums.length; i++){
-        if(nums.indexOf(i) === -1){
-            x = i
-        }
-    }
-    return x
+    let n = nums.length 
+    let realSum = n * (n + 1 ) / 2
+    for(let i=0; i < nums.length; i++){
+        realSum = realSum - nums[i]
+    }  
+    return realSum
 };
