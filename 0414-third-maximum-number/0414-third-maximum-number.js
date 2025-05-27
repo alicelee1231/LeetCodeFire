c731a4dca38ad618e19f0552a608a5cd7c9e8824
Alice <104006855+alicelee1231@users.noreply.github.com>
2025-05-27 13:34:36 +0900

    Time: 4 ms (49.69%), Space: 55.8 MB (65.37%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.js
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -3,22 +3,15 @@
  * @return {number}
  */
 var thirdMax = function(nums) {
-//     i need to find the third distinct maximum number.
-//     key point : distinct, third, maximum.
-//     regarding to thses key points, I need to use sort, max for checking the number of third maximum
-    // and Set for getting rid of duplicated factor in array.
-    nums.sort(function(a, b){
-            return b - a   
-        }
-    )
-    let a = 0
-    let x = new Set(nums)
-    if(x.size >= 3){
-//         Set can't be approched via index. so I need to convert it into array
-        a = Array.from(x)[2]
-    }else{
-        a = Math.max(...x)
 
-    }
-        return a
+const a = new Set([...nums])
+   const b = Array.from(a)
+   const x = b.sort(function(a,b){
+    return b - a
+   })
+   console.log(x)
+  if(x.length < 3){
+    return x[0]
+  }
+   return x[2]
 };
