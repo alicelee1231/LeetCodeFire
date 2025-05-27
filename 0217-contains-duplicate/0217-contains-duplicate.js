363289fb864b7f226bada46e21ff2f33e67d91ad
Alice <104006855+alicelee1231@users.noreply.github.com>
2025-05-28 07:17:34 +0900

    Time: 19 ms (46.04%), Space: 76.6 MB (19.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,13 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let a = new Map()
-    for(let i = 0; i < nums.length; i++){
-        if(a.has(nums[i])){
-            return true
-        }else{
-            a.set(nums[i], 1)
-        }
+  let a = new Map()
+  for(let i = 0; i < nums.length; i++){
+    if(a.has(nums[i])){
+        return true
+    }else{
+        a.set(nums[i], a.get(nums[i] + 1))
     }
-    return false
+  }
+  return false
 };
