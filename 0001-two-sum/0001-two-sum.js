bb2ac2c60361817cb085bf7973d593daf8abf798
Saemmi <104006855+alicelee1231@users.noreply.github.com>
2025-09-17 17:07:45 +0900

    Time: 49 ms (17.2%), Space: 54.5 MB (66.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,12 +6,11 @@
 var twoSum = function(nums, target) {
     let a = []
     for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++ ){
+        for(let j = i+1; j < nums.length; j++){
             if(nums[i] + nums[j] === target){
-                a.push(i)
-                a.push(j)
-                return a
+                a.push(i,j)
             }
         }
     }
+    return a
 };
