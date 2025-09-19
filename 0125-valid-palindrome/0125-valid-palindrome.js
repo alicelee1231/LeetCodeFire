51c759825d6210d1a1238ef825144243d575a3b1
Saemmi <104006855+alicelee1231@users.noreply.github.com>
2025-09-19 14:53:33 +0900

    Time: 2 ms (93.4%), Space: 58 MB (56.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,19 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s = s.toLowerCase()
-    let a = s.split(" ").join('')
-    let x = a.replace(/[^a-zA-Z0-9 ]/g, "")
-    console.log(x)
-    if(x.length === 0 || x.length === 1) return true
-    let y = 1
-    for(let i = 0; i < Math.floor(x.length / 2); i++){
-        if(x[i] != x[x.length - y]){
-            return false
-        }else if(x[i] === x[x.length - y]){
-            y++
-            continue
-        }
+    let b = s.replace(/[^\w]|_/g, "").toLowerCase()
+    let a = b.split("").reverse().join("")
+
+    if(s.trim().length === 0){
+        return true
+    }else if(b === a){
+        return true
     }
-    return true
+    return false
 };
